f8b51d66bba4131ddc84548029ffcada4cb45d25
Gringo <maxi@xcapit.com>
2025-08-13 17:47:17 -0300

    feat: Add contracts

--- contracts/src/Counter.sol
+++ contracts/src/Counter.sol
@@ -1,14 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract Token is Ownable {
+
+    using ECDSA for bytes32;
+    using MessageHashUtils for bytes32;
+
+    error InvalidSigner();
+    error InvalidNonce(address user, uint nonce);
+
+    mapping (address => uint) nonces;
+
+    constructor(address initialOwner) Ownable(initialOwner) {}
+
+    function claim(uint amount, uint nonce, bytes calldata signature) public {
+      if (nonceOf(msg.sender) != nonce) revert InvalidNonce(msg.sender, nonce);
+      if (keccak256(abi.encodePacked(msg.sender, amount, nonce))
+        .toEthSignedMessageHash()
+        .recover(signature) == owner()) {
+          incrementNonceOf(msg.sender);
+          // do the transfer....
+        } else revert InvalidSigner();
     }
 
-    function increment() public {
-        number++;
+    function incrementNonceOf(address aUser) private {
+      nonces[aUser] += 1;
+    }
+
+    function nonceOf(address aUser) public view returns (uint) {
+      return nonces[aUser];
     }
 }
--- contracts/test/Counter.t.sol
+++ contracts/test/Counter.t.sol
@@ -1,24 +1,86 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {Test, console, Vm} from "forge-std/Test.sol";
+import {Token} from "../src/Counter.sol";
+import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+
+    using ECDSA for bytes32;
+    using MessageHashUtils for bytes32;
+
+    Token public token;
+    Vm.Wallet public bob;
+    Vm.Wallet public owner;
+    Vm.Wallet public lisa;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        owner = vm.createWallet("owner");
+        bob = vm.createWallet("bob");
+        lisa = vm.createWallet("lisa");
+        token = new Token(owner.addr);
+    }
+    
+    function test_nonceOf() public view {
+      assertEq(0, token.nonceOf(lisa.addr));
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function test_ClaimInvalidSigner() public {
+      uint amount = 2;
+      uint nonce = 0;
+
+      bytes memory signature = _offChainBadSignature(bob.addr, amount, nonce);
+      vm.expectRevert(Token.InvalidSigner.selector);
+
+      vm.startPrank(bob.addr);
+      token.claim(amount, nonce, signature);
+      vm.stopPrank();
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function test_ClaimInvalidNonce() public {
+      uint amount = 2;
+      uint nonce = 1;
+
+      bytes memory signature = _offChainSignature(bob.addr, amount, nonce);
+
+      vm.expectRevert(
+        abi.encodeWithSelector(Token.InvalidNonce.selector, bob.addr, 1)
+      );
+
+      vm.startPrank(bob.addr);
+      token.claim(amount, nonce, signature);
+      vm.stopPrank();
+    }
+
+    function test_ClaimOk() public {
+      uint amount = 2;
+      uint nonce = 0;
+
+      bytes memory signature = _offChainSignature(bob.addr, amount, nonce);
+
+      vm.startPrank(bob.addr);
+      token.claim(amount, nonce, signature);
+      vm.stopPrank();
+
+      assertEq(token.nonceOf(bob.addr), 1);
+    }
+    
+    function _offChainBadSignature(address aUser, uint amount, uint nonce) private returns (bytes memory) {
+      return _signature(aUser, amount, nonce, lisa);
+    }
+
+    function _offChainSignature(address aUser, uint amount, uint nonce) private returns (bytes memory) {
+      return _signature(aUser, amount, nonce, owner);
+    }
+
+    function _signature(address aUser, uint amount, uint nonce, Vm.Wallet memory signer) private returns (bytes memory) {
+      (uint8 v, bytes32 r, bytes32 s) = vm.sign(
+        signer, 
+        keccak256(abi.encodePacked(aUser, amount, nonce)).toEthSignedMessageHash()
+      );
+      return abi.encodePacked(r, s, v);
     }
 }
+
